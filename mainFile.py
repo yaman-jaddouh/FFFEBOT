efab6a0f8188866972e0a5202b0cae375cac8ac9
yaman jaddouh <yamanlem@hotmail.com>
2020-11-01 09:54:37 +0200

    adding history func and history.json

--- mainFile.py
+++ mainFile.py
@@ -1,24 +1,40 @@
 #!/usr/ env python
 # -*- coding: utf-8 -*-
 import telebot
-from subFile import check_or_add,check_old
-from telebot import types
-
+from telebot.types import KeyboardButton , ReplyKeyboardMarkup
+from subFile import check_old,check_or_add,historyW
 
 bot=telebot.TeleBot('1117323096:AAFjgXAykVD7T4qMPFsWo35mBQZV2YfIgzo')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, text="Hi there")
-    bot.send_message(message.chat.id, text=str(message.from_user.id))
+    
+    key = ReplyKeyboardMarkup(one_time_keyboard = True , resize_keyboard = True)
+    b1 = KeyboardButton("المحاضرات 📝")
+    b2 = KeyboardButton("حول البوت⁉️")
+    b3=KeyboardButton("دورات 🖌🗒")
+    b4=KeyboardButton("مراجع وكتب عامة  📚")
+    key.add(b1,b3)
+    key.add(b4)
+    key.add(b2)
+    bot.send_message(message.chat.id,text=" أهلا وسهلا بك 💚 ",reply_markup=key)
+    historyW(message.from_user.id,[])
     check_or_add(message.from_user.id)
+
+
+
+
+
+
 
 
 @bot.message_handler(content_types=["text"])
 def replay(message):
     a=check_old(message.from_user.id)
     if not a :
-        bot.send_message(message.chat.id,text=" انت تستخدم اخر اصدار من البوت ")
+        if message.text =='حول البوت⁉️' :
+            bot.send_message(message.chat.id,text="FEEEBot v 1.0.0\nبوت مساعد لكل طلاب الكهرباء في جامعة حلب\nيحتوي على محاضرات ودورات\nفي حالة وجود اي اقتراحات [أرجو إعلامنا بها](t.me/Yaman_jaddouh)\n\nby [Yaman jaddouh](t.me/Yaman_jaddouh)",parse_mode="Markdown")
+            return
     else:
         bot.send_message(message.chat.id,text="انت تستخدم اصدار قديم من البوت الرجاء اضغط على \n/start\nلتحديث البوت ...وشكراً")
      
--- subFile.py
+++ subFile.py
@@ -1,7 +1,22 @@
 #!/usr/ env python
 # -*- coding: utf-8 -*-
 import json
-a={ 'users':[123,434]}
+def historyW(id,array):
+    a={}
+    fileR=open('history.json','r')
+    a=json.loads(fileR.read())
+    if str(id) not in a:
+        a[id]=array
+    else:
+        a[str(id)]=array
+    fileW=open('history.json','w')
+    fileW.write(json.dumps(a))
+    fileW.close()
+
+def historyR(id):
+    pass
+
+#historyW(880941251,[1,23,4,2])
 
 def check_old(id):
     fileR=open('data.json','r')
@@ -18,6 +33,8 @@ def check_or_add(id): #check or add user if exist or not
     #result=False
     fileR=open('data.json','r')
     data=json.loads(fileR.read())
+    if check_old(id) :
+        data['old_users'].remove(id)
     if id in data['users']:
         return True
     else:
